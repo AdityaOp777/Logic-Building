9eaf52e7e5d7a3298986603c71d13b293a79bf60
AdityaOp777 <adityagavhane91@gmail.com>
2025-05-19 15:35:42 +0530

    Add files via upload

---
+++ program63.c
@@ -0,0 +1,32 @@
+/*
+    A A A A A
+    B B B B B
+    C C C C C
+*/
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%c\t",i + 64);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of Rows And Columns : ");
+    scanf("%d \t %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+ 
+    return 0;
+}
